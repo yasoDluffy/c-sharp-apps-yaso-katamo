3d8bcbf0624f0f060d63e8497edb5acd366450c7
yaso <145655778+yasoDluffy@users.noreply.github.com>
2024-05-24 09:36:12 +0300

    Update Program.cs

--- c-sharp-apps-yaso-katamo/Program.cs
+++ c-sharp-apps-yaso-katamo/Program.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace c_sharp_apps_yaso_katamo
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
         }
     }
