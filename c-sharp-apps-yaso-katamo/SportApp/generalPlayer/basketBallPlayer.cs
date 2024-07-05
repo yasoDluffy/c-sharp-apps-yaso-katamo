fa1ee6abe4bb8960f6ec363f7b79023c0592e659
yasoDluffy <yaso26849@gmail.com>
2024-07-05 11:33:21 +0300

    practice bank

---
+++ c-sharp-apps-yaso-katamo/SportApp/generalPlayer/basketBallPlayer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_apps_yaso_katamo.SportApp.generalPlayer
+{
+    public class  basketBallPlayer : genral_Player
+    {
+        private int Dunks;
+        private int threeShots;
+        private int rebounds;
+
+        public basketBallPlayer(string scoreName) : base("basket")
+        {
+            this.Dunks = 0;
+            this.threeShots = 0;
+            this.rebounds = 0;
+        }
+        
+        public override void  AddFoul()
+        {
+            this.fouls++;
+            if(this.fouls == 5)
+            {
+                this.outOfGame= true;
+                Console.WriteLine("you out of the game");
+            }
+        }
+        public override void ScoreField()
+        {
+            this.points += 2;
+            DisplayScore();
+        }
+        public void Score3()
+        {
+            this.threeShots += 3;
+        }
+
+    }
+}
